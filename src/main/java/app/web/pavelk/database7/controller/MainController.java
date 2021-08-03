82b1b1e61d5c843ea529ec266d9882ac2b53d741
PavelK6896 <79208776896@yandex.ru>
2021-08-03 20:32:18 +0300

    update 3

--- src/main/java/app/web/pavelk/database7/controller/MainController.java
+++ src/main/java/app/web/pavelk/database7/controller/MainController.java
@@ -4,36 +4,46 @@ package app.web.pavelk.database7.controller;
 import app.web.pavelk.database7.repo.BookRepo;
 import app.web.pavelk.database7.shema.Book;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-@Controller
+import java.io.IOException;
+
+@RestController
 @RequiredArgsConstructor
 public class MainController {
 
     private final BookRepo bookRepo;
 
+    @Value("classpath:static/favicon.ico")
+    Resource resource;
+
     @GetMapping("/")
-    @ResponseBody
     public String main() {
         return bookRepo.findAll().toString();
     }
 
     @GetMapping("/s")
-    @ResponseBody
     public String main2() {
         bookRepo.save(Book.builder().build());
         return "ok";
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
     public String main3(@PathVariable("id") Long id) {
         return bookRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)).toString();
+    }
+
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    public byte[] returnNoFavicon() throws IOException {
+        return resource.getInputStream().readAllBytes();
     }
 
 }
--- src/main/java/app/web/pavelk/database7/shema/Book.java
+++ src/main/java/app/web/pavelk/database7/shema/Book.java
@@ -4,6 +4,8 @@ import lombok.*;
 
 import javax.persistence.*;
 
+import java.math.BigInteger;
+
 import static javax.persistence.GenerationType.IDENTITY;
 
 @Data
