eb64e4465afa4bbcdd19f91226d113f095548af3
PavelK6896 <79208776896@yandex.ru>
2021-08-05 20:16:01 +0300

    update 5

--- src/main/java/app/web/pavelk/database7/component/ScheduledCom1.java
+++ src/main/java/app/web/pavelk/database7/component/ScheduledCom1.java
@@ -1,12 +1,19 @@
 package app.web.pavelk.database7.component;
 
+import app.web.pavelk.database7.repo.BookRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Scheduled;
 
+@RequiredArgsConstructor
 public class ScheduledCom1 {
+
+    private final BookRepo bookRepo;
 
     @Scheduled(fixedDelay = 1000)
     public void scheduledCom1() {
-        System.out.println("scheduledCom1");
+        System.out.println(bookRepo.findAll(PageRequest.of(0, 1, Sort.by(Sort.Direction.DESC, "id"))).getContent());
     }
 
 }
--- src/main/java/app/web/pavelk/database7/repo/BookRepo.java
+++ src/main/java/app/web/pavelk/database7/repo/BookRepo.java
@@ -2,6 +2,8 @@ package app.web.pavelk.database7.repo;
 
 
 import app.web.pavelk.database7.shema.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,4 +12,7 @@ public interface BookRepo extends JpaRepository<Book, Long> {
 
     @Override
     Optional<Book> findById(Long aLong);
+
+    Page<Book> findAll(Pageable pageable);
+
 }
